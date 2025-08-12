b53fb03e9257899a0a8faaf7b274552d43627f2d
AdmiralKeso <cas.hagenaar@gmail.com>
2025-08-12 18:39:30 +0200

    Create player movement script

---
+++ autumn-soup/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f; // Movement speed in units/second
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input from WASD / arrow keys
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Normalize so diagonal movement isn't faster
+        movement = movement.normalized;
+
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
