2d996ec0c53b79eee6c206356e80f35e4be820ec
AdmiralKeso <cas.hagenaar@gmail.com>
2025-08-19 15:43:34 +0200

    Max count of items to 6

--- autumn-soup/Assets/InventoryManager.cs
+++ autumn-soup/Assets/InventoryManager.cs
@@ -6,6 +6,7 @@ public class InventoryManager : MonoBehaviour
     public static InventoryManager Instance;
 
     public List<string> items = new List<string>();
+    public int maxItems = 6; // Maximum items allowed
 
     private void Awake()
     {
@@ -23,6 +24,12 @@ public class InventoryManager : MonoBehaviour
     // Adds a new item to the inventory
     public void AddItem(string itemName)
     {
+        if (items.Count >= maxItems)
+        {
+            Debug.LogWarning("Inventory full! Cannot add: " + itemName);
+            return;
+        }
+
         items.Add(itemName);
         Debug.Log("Added: " + itemName);
     }
@@ -30,7 +37,10 @@ public class InventoryManager : MonoBehaviour
     // Removes an item (optional)
     public void RemoveItem(string itemName)
     {
-        items.Remove(itemName);
-        Debug.Log("Removed: " + itemName);
+        if (items.Contains(itemName))
+        {
+            items.Remove(itemName);
+            Debug.Log("Removed: " + itemName);
+        }
     }
 }
